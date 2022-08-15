52fd42507464a5ab33e71d46a9dfcce63eba9020
Tony Padmore <emperialace@gmail.com>
2022-08-15 05:41:28 -0400

    List shows due to return

--- frontend/src/component/pages/Main.js
+++ frontend/src/component/pages/Main.js
@@ -1,29 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import classes from './Main.module.css';
-import { NavLink, Nav} from 'react-bootstrap';
+import {  Nav} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import uuid from 'react-uuid';
 
 
 
-function Main(){
+ function Main(){
 
-    const url = 'http://localhost:3000/main/62f9740fa7af0efb8e1b628b';
+    const url = 'http://localhost:3000/main/62fa13de63acff90fc88ef1b';
     const [foodRecipe, setFoodRecipe] = useState([]);
+
 
     useEffect(() => {
         axios.get(url).then(res => {
             setFoodRecipe((res.data));
            
-            console.log(foodRecipe)
+            //console.log(foodRecipe)
         })
     }, [])
 
-    const items =  foodRecipe.ingredient;
-    const theItems =foodRecipe.ingredient.map((d)=> {
-        <li key={d._id}>{d.value}</li>
-    })
+    let items =  foodRecipe.ingredient;
+    
+    
     //const lineItem = JSON.parse(items);
     /*const listItems = JSON.parse(items);*/
     //JSON.parse(items);
@@ -42,46 +41,38 @@ function Main(){
 
     
 
-    return (
-        <div>
-            <div className={classes.navCen}>
-                <Nav className={classes.navs}>
-                    <Nav.Item>
-                        <Link to='/create'>Add Recipe</Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="Removing recipe">Remove Recipe</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </div>
-                
+        return (
             <div>
-                <h1 className={classes.header}>Recipes are here</h1>
-                <p> {foodRecipe.name}</p>
-                <div>
-                    <ul>
-
-                      return(
-                        {items && items.map((d) => {
-
-                            <li key={d._id}> {d.value}</li>
-                        }
-
-                        )}
-                      )  
-                      
-                        
-                      
-
-                    </ul>
+                <div className={classes.navCen}>
+                    <Nav className={classes.navs}>
+                        <Nav.Item>
+                            <Link to='/create'>Add Recipe</Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="Removing recipe">Remove Recipe</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
                 </div>
-              
+                    
+                <div>
+                    <h1 className={classes.header}>Recipes are here</h1>
+                    <p> {foodRecipe.name}</p>
+                    <div>
+                        <ul>     
+                            {items && items.map((d) => {
+                                return (
+                                  <li key = {d._id}>{d.value}</li>  
+                                );
+                            })}                  
+                        </ul>
+                    </div>
+                
+                </div>
+                
             </div>
             
-        </div>
-        
-        
-    )
+            
+        )
 }
 
 export default Main;
